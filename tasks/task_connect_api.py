2f95cbaa645272bf52fec22dfcf792a3c30c11e8
charlesyjchen <charlesyjchen1@gmail.com>
2021-09-22 00:03:18 -0400

    feat: connect to api, test with drake

--- tasks/task_connect_api.py
+++ tasks/task_connect_api.py
@@ -1,5 +1,21 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+os.environ["SPOTIPY_CLIENT_ID"] = "094f39d25a5d45c3b0671baa71d58425"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "bbd8a243877d4092ba5774484b41dc2e"
+
+def getTracks(query: str):
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'],
+                                                            client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
+
+    results = sp.search(q=query, limit=20)   
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
+    return results
+
 def sampleFunction(input):
     output = input + 2
-    print(output)
+    return output
 
-sampleFunction(3)
+getTracks('drake')
--- tests/unit_tests/test_connect_api.py
+++ tests/unit_tests/test_connect_api.py
@@ -1,7 +1,12 @@
-from synestify.task_connect_api.py import sampleFunction
+from tasks.task_connect_api import sampleFunction, getTracks
 
 class TestAPIConnection():
 
     def test_sample_function(self):
         output = sampleFunction(3)
         assert output == 5
+
+    def test_get_tracks(self):
+        track_list = getTracks('drake')
+        assert track_list['tracks']['items'][0]['name'] == 'Way 2 Sexy (with Future & Young Thug)'
+        assert len(track_list['tracks']['items']) == 20
