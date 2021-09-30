6bdedbb6fe08e9f7457dcd3d1c287e74297f013e
lawrencehhan <57281309+lhh128@users.noreply.github.com>
2021-09-30 00:33:34 -0700

    added sample image, created draft analysis script with functions that convert rgb to hsl. Currently the conversion takes a very long time

---
+++ image_analysis/image_analysis.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import numpy as np
+import scipy
+
+img = Image.open('test_image_01_kyoto.jpg')
+img.show()
+
+imgar = np.asarray(img)
+imgar.shape # (4903, 3262, 3) = (height pos, width pos, rgb)
+
+
+# Returns normalized rgb np.array
+def normalize_rgb(rgb):
+    rgb_n = [val/255 for val in rgb]
+    return rgb_n  
+
+# Takes an rgb array [r,g,b] and returns its hsl (hue, sat%, light%) equivalent 
+def rgb_to_hsl_pixel(rgb):
+    rgb_n = normalize_rgb(rgb)
+    r, g, b = rgb_n[0], rgb_n[1], rgb_n[2] # normalized rgb values split
+
+    max_rgb = max(r,g,b)
+    min_rgb = min(r,g,b)
+    chroma = max_rgb - min_rgb # 'Roughly the dist from the point of origin' (radial dimension) Note: normalized
+
+    ## Hue Calculation
+    hue_prime_pw = { # Piecewise mathetmical definiion of hue' (aka length of RYGCP)
+        #r: lambda c: ((g-b)/c) % 6,
+        r: lambda c: ((g-b)/c) + 6 if ((g-b)/c)<0 else ((g-b)/c), # 360/60 = 6 = deg shift for R° @ 0° to bring the segment above 0°
+        g: lambda c: ((b-r)/c) + 2, # 120/60 = 2 = deg shift for G° @ 120° w/ ea hex side being 60°
+        b: lambda c: ((r-g)/c) + 4, # 240/60 = 4 = deg shift for B° @ 240° w/ ea hex side being 60°
+        0: lambda c: c
+    }
+
+    hue_prime = hue_prime_pw[max_rgb](chroma) # Applying appropriate hue piecewise func given max_rgb
+    h = hue_prime * 60 # unit: hue°
+
+    ## Luminance Calculation: Average of largest and smallest RGB components
+    l = (max_rgb + min_rgb) / 2 * 100 # unit: luminance%
+    
+    ## Saturation Calculation: Chroma scaled to fill [0,1]
+    if chroma == 0: # max and min are the same => no saturation
+        s = 0
+    else:
+        s = chroma / (1 - abs(2*(l/100) - 1)) * 100 # unit: saturation%
+
+    hsl = np.asarray([h, s, l])
+    return hsl
+
+
+# Converts every pixel in an array type image from RGB to HSL
+def rgb_to_hsl_imgar(imgar):
+    img_height = imgar.shape[0]
+    img_width = imgar.shape[1]
+    hsl_imgar = np.zeros([img_height, img_width, 3])
+
+    for h in range(img_height):
+        for w in range(img_width):
+            rgb_pixel = imgar[h, w]
+            hsl_pixel = rgb_to_hsl_pixel(rgb_pixel)
+            
+            hsl_imgar[h, w] = hsl_pixel
+
+            print(f'Pixel @ {h}x{w}: {hsl_pixel}')
+    
+    return hsl_imgar
