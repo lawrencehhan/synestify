9549186bae38a49a274105a443c768f97877b3b8
charlesyjchen <charlesyjchen1@gmail.com>
2022-01-24 02:25:50 -0500

    Add user-input for favorite track and artist, create and invoke spotify api search endpoint, update readme

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # synestify
-## **Description**
 Synestify analyzes a user-uploaded image and user-inputted information to recommend songs curated to reflect the image's visual cues and user's interests. The analysis matches tonal, hue, brightness, user-data, etc. to musical values accepted by Spotify's public API to utilize Spotify's recommendation engine. 
 
 ## **Technical Workflow**
--- src/tasks/task_connect_api.py
+++ src/tasks/task_connect_api.py
@@ -19,34 +19,44 @@ def getSpotifyToken():
     bearer_token = (json.loads(response.content.decode("ascii")))['access_token']
     return bearer_token
 
-def sendGetRequest(bearer_token, url):
+def getGenreSeeds(bearer_token: str):
+    # Get list of genres: https://developer.spotify.com/console/get-available-genre-seeds/
+    url = "	https://api.spotify.com/v1/recommendations/available-genre-seeds"
+    genre_list = sendGetRequest(bearer_token, url)
+    return genre_list['genres']
+
+def getSearchResults(bearer_token: str, query: str, search_type:str, limit: int):
+    # Get recommended songs: https://developer.spotify.com/console/get-recommendations/
+    url = _createSearchUrl(query, search_type, limit)
+    search_results = sendGetRequest(bearer_token, url)
+    return search_results
+
+def getRecommendations(bearer_token: str, limit: int, market: str, seed_artists: str, seed_genres: str, seed_tracks: str):
+    # Get recommended songs: https://developer.spotify.com/console/get-recommendations/
+    url = _createRecommendationsUrl(limit, market, seed_artists, seed_genres, seed_tracks)
+    recommendations = sendGetRequest(bearer_token, url)
+    return recommendations
+
+def sendGetRequest(bearer_token: str, url: str):
     headers = {"Authorization": "Bearer " + bearer_token}
     response = requests.get(url, headers=headers)
     response_content = response.json()
     return response_content
 
-def createRecommendationsUrl(limit, market, seed_artists, seed_genres, seed_tracks):
+def _createSearchUrl(query: str, search_type: str, limit: int):
+    base_url = "https://api.spotify.com/v1/search?"
+    query = "q=" + query.replace(" ", "%20")
+    search_type = "&type=" + search_type
+    limit = "&limit=" + str(limit)
+    search_url = base_url + query + search_type + limit
+    return search_url
+
+def _createRecommendationsUrl(limit: int, market: str, seed_artist: str, seed_genre: str, seed_track: str):
     base_url = "https://api.spotify.com/v1/recommendations?"
-    limit = "limit=3"
+    limit = "limit=" + str(limit)
     market = "&market=US"
-    seed_artists = "&seed_artists=3TVXtAsR1Inumwj472S9r4" # Drake
-    seed_genres = "&seed_genres=" + seed_genres
-    seed_tracks = "&seed_tracks=7wcWkzT1X75DguAwOWxlGt" # Way 2 Sexy
-    recommendations_url = base_url + limit + market + seed_artists + seed_genres + seed_tracks
+    seed_artist = "&seed_artists=" + seed_artist # Example: 3TVXtAsR1Inumwj472S9r4 Drake
+    seed_genre = "&seed_genres=" + seed_genre # Example: pop
+    seed_track = "&seed_tracks=" + seed_track # Example: 7wcWkzT1X75DguAwOWxlGt Way 2 Sexy
+    recommendations_url = base_url + limit + market + seed_artist + seed_genre + seed_track
     return recommendations_url
-
-def getRecommendations(bearer_token, limit, market, seed_artists, seed_genres, seed_tracks):
-    # Get recommended songs: https://developer.spotify.com/console/get-recommendations/
-    url = createRecommendationsUrl(limit, market, seed_artists, seed_genres, seed_tracks)
-    recommendations = sendGetRequest(bearer_token, url)
-    return recommendations
-
-def getArtistIds(): 
-    # Search for artist seed ids https://api.spotify.com/v1/search
-    return 
-
-def getGenreSeeds(bearer_token):
-    # Get list of genres: https://developer.spotify.com/console/get-available-genre-seeds/
-    url = "	https://api.spotify.com/v1/recommendations/available-genre-seeds"
-    genre_list = sendGetRequest(bearer_token, url)
-    return genre_list['genres']
